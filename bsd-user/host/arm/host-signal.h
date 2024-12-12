5741ca9fa9bbae23c452b67dc7526be660c42787
Maksym Sobolyev <sobomax@sippysoft.com>
2024-12-12 16:42:47 +0000

    Stubify to make compatible woth linux/arm.

--- bsd-user/host/arm/host-signal.h
+++ bsd-user/host/arm/host-signal.h
@@ -13,12 +13,20 @@
 
 static inline uintptr_t host_signal_pc(ucontext_t *uc)
 {
+#if !defined(__linux__)
     return uc->uc_mcontext.__gregs[_REG_PC];
+#else
+    abort();
+#endif
 }
 
 static inline void host_signal_set_pc(ucontext_t *uc, uintptr_t pc)
 {
+#if !defined(__linux__)
     uc->uc_mcontext.__gregs[_REG_PC] = pc;
+#else
+    abort();
+#endif
 }
 
 static inline bool host_signal_write(siginfo_t *info, ucontext_t *uc)
@@ -27,9 +35,13 @@ static inline bool host_signal_write(siginfo_t *info, ucontext_t *uc)
      * In the FSR, bit 11 is WnR. FreeBSD returns this as part of the
      * si_info.si_trapno.
      */
+#if !defined(__linux__)
     uint32_t fsr = info->si_trapno;
 
     return extract32(fsr, 11, 1);
+#else
+    abort();
+#endif
 }
 
 #endif
