af61751fa478390d3988fb7fc5d9bd130c490725
Maksym Sobolyev <sobomax@sippysoft.com>
2024-12-24 21:51:06 +0000

    If apt install fails, try to run apt update and repeat.

--- bsd-user/scripts/ci/pre-build.sh
+++ bsd-user/scripts/ci/pre-build.sh
@@ -3,7 +3,14 @@
 set -e
 
 install_clang() {
-  ${APT_INSTALL} curl gpg lsb-release ca-certificates
+  local i=0
+  while true
+  do
+    ${APT_INSTALL} curl gpg lsb-release ca-certificates && break
+    test ${i} -gt 1 && exit 1
+    ${APT_UPDATE}
+    local i=$((${i} + 1))
+  done
   MYREL="`lsb_release -c | grep ^Codename | awk '{print $2}'`"
   echo "deb [signed-by=/usr/share/keyrings/llvm.gpg] http://apt.llvm.org/${MYREL}/ llvm-toolchain-${MYREL}-${CLANG_VER} main" > /etc/apt/sources.list.d/llvm.list
   curl https://apt.llvm.org/llvm-snapshot.gpg.key | gpg --dearmor > /usr/share/keyrings/llvm.gpg
